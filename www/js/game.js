17c2cd342036cd10d97c6eb5c0fb87631352c05e
Christopher Franklin <chrisf0321@gmail.com>
2015-01-27 19:32:52 -0500

    touch

--- www/js/game.js
+++ www/js/game.js
@@ -1,3 +1,60 @@
+var hasTouch = ('ontouchstart' in window);
+var TOUCH_START = hasTouch ? "touchstart" : "mousedown";
+var gamCnt = 0;
+var imgCnt = 0;
+var aCnt = 0;
+var bCnt = 0;
+var cCnt = 0;
+var timer;
+var hide = false;
+var choice1;
+var choice2;
+var prevSel;
+
+var aArry = ["img/a1.jpg", "img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a5.jpg", "img/a6.jpg", "img/a7.jpg",
+            "img/a8.jpg", "img/a9.jpg", "img/a10.jpg", "img/a11.jpg", "img/a12.jpg", "img/a13.jpg", "img/a14.jpg",
+            "img/a15.jpg", "img/a16.jpg", "img/a17.jpg", "img/a18.jpg", "img/a19.jpg", "img/a20.jpg", "img/a21.jpg",
+            "img/a22.jpg", "img/a23.jpg", "img/a24.jpg", "img/a25.jpg", "img/a26.jpg", "img/a27.jpg", "img/a28.jpg",
+            "img/a29.jpg", "img/a30.jpg", "img/a31.jpg", "img/a32.jpg"];
+        
+var bArry = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg", "img/b5.jpg", "img/b6.jpg", "img/b7.jpg",
+            "img/b8.jpg", "img/b9.jpg", "img/b10.jpg", "img/b11.jpg", "img/b12.jpg", "img/b13.jpg", "img/b14.jpg",
+            "img/b15.jpg", "img/b16.jpg", "img/b17.jpg", "img/b18.jpg", "img/b19.jpg", "img/b20.jpg", "img/b21.jpg",
+            "img/b22.jpg", "img/b23.jpg", "img/b24.jpg", "img/b25.jpg", "img/b26.jpg", "img/b27.jpg", "img/b28.jpg",
+            "img/b29.jpg", "img/b30.jpg", "img/b31.jpg", "img/b32.jpg", "img/b33.jpg", "img/b34.jpg", "img/b35.jpg", 
+            "img/b36.jpg", "img/b37.jpg", "img/b38.jpg", "img/b39.jpg", "img/b40.jpg"];
+        
+var cArry = ["img/c1.jpg", "img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg", "img/c6.jpg", "img/c7.jpg",
+            "img/c8.jpg", "img/c9.jpg", "img/c10.jpg", "img/c11.jpg", "img/c12.jpg", "img/c13.jpg", "img/c14.jpg",
+            "img/c15.jpg", "img/c16.jpg", "img/c17.jpg", "img/c18.jpg", "img/c19.jpg", "img/c20.jpg", "img/c21.jpg",
+            "img/c22.jpg", "img/c23.jpg", "img/c24.jpg", "img/c25.jpg", "img/c26.jpg", "img/c27.jpg", "img/c28.jpg",
+            "img/c29.jpg", "img/c30.jpg", "img/c31.jpg", "img/c32.jpg", "img/c33.jpg", "img/c34.jpg", "img/c35.jpg", 
+            "img/c36.jpg", "img/c37.jpg", "img/c38.jpg", "img/c39.jpg", "img/c40.jpg", "img/c41.jpg", "img/c42.jpg", 
+            "img/c43.jpg", "img/c44.jpg", "img/c45.jpg", "img/c46.jpg", "img/c47.jpg", "img/c48.jpg"];
+        
+var dArry = ["img/d1.jpg", "img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d5.jpg", "img/d6.jpg", "img/d7.jpg",
+            "img/d8.jpg", "img/d9.jpg", "img/d10.jpg", "img/d11.jpg", "img/d12.jpg", "img/d13.jpg", "img/d14.jpg",
+            "img/d15.jpg", "img/d16.jpg", "img/d17.jpg", "img/d18.jpg", "img/d19.jpg", "img/d20.jpg", "img/d21.jpg",
+            "img/d22.jpg", "img/d23.jpg", "img/d24.jpg", "img/d25.jpg", "img/d26.jpg", "img/d27.jpg", "img/d28.jpg",
+            "img/d29.jpg", "img/d30.jpg", "img/d31.jpg", "img/d32.jpg", "img/d33.jpg", "img/d34.jpg", "img/d35.jpg", 
+            "img/d36.jpg", "img/d37.jpg", "img/d38.jpg", "img/d39.jpg", "img/d40.jpg", "img/d41.jpg"];
+
+var eArry = ["img/e1.jpg", "img/e2.jpg", "img/e3.jpg", "img/e4.jpg"];
+
+var matchArry = [];
+var trialMtch = [];
+
+$("a").on(TOUCH_START, function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var linkID = $(this).attr('id');
+    
+    if (linkID) {
+        var num = parseInt(linkID);
+        countClick(num);
+    }
+});
+
 $(document).on('pagebeforeshow', '#matches', function() {
     $('.ui-block-a, .ui-block-b').removeClass('inst_mod');
     $('img').css({'opacity': '0'});
@@ -18,39 +75,6 @@ $(document).on('pagebeforeshow', '#matches', function() {
 
 
 $(document).on('pagebeforeshow', '#game', function () {
-    $("#l1").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(0);
-    });
-    $("#l2").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(1);
-    });
-    $("#l3").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(2);
-    });
-    $("#l4").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(3);
-    });
-    $("#l5").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(4);
-    });
-    $("#l6").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(5);
-    });
-    $("#l7").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(6);
-    });
-    $("#l8").bind('vclick', function(e) {
-        e.preventDefault();
-        countClick(7);
-    });
-
     $('.ui-block-a, .ui-block-b').removeClass('inst_mod');
     gameCnt = 0;
     imgCnt = 0;
@@ -103,50 +127,6 @@ $(document).on('pagebeforeshow', '#inst2', function(){
     $('.ui-block-a, .ui-block-b').addClass('inst_mod');
 });
 
-var gamCnt = 0;
-var imgCnt = 0;
-var aCnt = 0;
-var bCnt = 0;
-var cCnt = 0;
-var timer;
-var hide = false;
-var choice1;
-var choice2;
-var prevSel;
-
-var aArry = ["img/a1.jpg", "img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a5.jpg", "img/a6.jpg", "img/a7.jpg",
-            "img/a8.jpg", "img/a9.jpg", "img/a10.jpg", "img/a11.jpg", "img/a12.jpg", "img/a13.jpg", "img/a14.jpg",
-            "img/a15.jpg", "img/a16.jpg", "img/a17.jpg", "img/a18.jpg", "img/a19.jpg", "img/a20.jpg", "img/a21.jpg",
-            "img/a22.jpg", "img/a23.jpg", "img/a24.jpg", "img/a25.jpg", "img/a26.jpg", "img/a27.jpg", "img/a28.jpg",
-            "img/a29.jpg", "img/a30.jpg", "img/a31.jpg", "img/a32.jpg"];
-        
-var bArry = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg", "img/b5.jpg", "img/b6.jpg", "img/b7.jpg",
-            "img/b8.jpg", "img/b9.jpg", "img/b10.jpg", "img/b11.jpg", "img/b12.jpg", "img/b13.jpg", "img/b14.jpg",
-            "img/b15.jpg", "img/b16.jpg", "img/b17.jpg", "img/b18.jpg", "img/b19.jpg", "img/b20.jpg", "img/b21.jpg",
-            "img/b22.jpg", "img/b23.jpg", "img/b24.jpg", "img/b25.jpg", "img/b26.jpg", "img/b27.jpg", "img/b28.jpg",
-            "img/b29.jpg", "img/b30.jpg", "img/b31.jpg", "img/b32.jpg", "img/b33.jpg", "img/b34.jpg", "img/b35.jpg", 
-            "img/b36.jpg", "img/b37.jpg", "img/b38.jpg", "img/b39.jpg", "img/b40.jpg"];
-        
-var cArry = ["img/c1.jpg", "img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg", "img/c6.jpg", "img/c7.jpg",
-            "img/c8.jpg", "img/c9.jpg", "img/c10.jpg", "img/c11.jpg", "img/c12.jpg", "img/c13.jpg", "img/c14.jpg",
-            "img/c15.jpg", "img/c16.jpg", "img/c17.jpg", "img/c18.jpg", "img/c19.jpg", "img/c20.jpg", "img/c21.jpg",
-            "img/c22.jpg", "img/c23.jpg", "img/c24.jpg", "img/c25.jpg", "img/c26.jpg", "img/c27.jpg", "img/c28.jpg",
-            "img/c29.jpg", "img/c30.jpg", "img/c31.jpg", "img/c32.jpg", "img/c33.jpg", "img/c34.jpg", "img/c35.jpg", 
-            "img/c36.jpg", "img/c37.jpg", "img/c38.jpg", "img/c39.jpg", "img/c40.jpg", "img/c41.jpg", "img/c42.jpg", 
-            "img/c43.jpg", "img/c44.jpg", "img/c45.jpg", "img/c46.jpg", "img/c47.jpg", "img/c48.jpg"];
-        
-var dArry = ["img/d1.jpg", "img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d5.jpg", "img/d6.jpg", "img/d7.jpg",
-            "img/d8.jpg", "img/d9.jpg", "img/d10.jpg", "img/d11.jpg", "img/d12.jpg", "img/d13.jpg", "img/d14.jpg",
-            "img/d15.jpg", "img/d16.jpg", "img/d17.jpg", "img/d18.jpg", "img/d19.jpg", "img/d20.jpg", "img/d21.jpg",
-            "img/d22.jpg", "img/d23.jpg", "img/d24.jpg", "img/d25.jpg", "img/d26.jpg", "img/d27.jpg", "img/d28.jpg",
-            "img/d29.jpg", "img/d30.jpg", "img/d31.jpg", "img/d32.jpg", "img/d33.jpg", "img/d34.jpg", "img/d35.jpg", 
-            "img/d36.jpg", "img/d37.jpg", "img/d38.jpg", "img/d39.jpg", "img/d40.jpg", "img/d41.jpg"];
-
-var eArry = ["img/e1.jpg", "img/e2.jpg", "img/e3.jpg", "img/e4.jpg"];
-
-var matchArry = [];
-var trialMtch = [];
-
 function showMatch() {
 
     $("#mImg1").attr('src', matchArry[0]);
@@ -161,7 +141,7 @@ function showMatch() {
 function imgDelay() {
     setTimeout(function() {
         $('img').css({'opacity': '100'});
-    }, 750);
+    }, 500);
 }
 
 Array.prototype.shuffle = function() {
@@ -232,7 +212,7 @@ function imgSel() {
         imgDelay();
     }
     else {
-        $.mobile.changePage("#finish", {transition: "pop"});
+        $.mobile.changePage("#finish", {transition: "turn"});
         resetGame();
     }
 }
@@ -243,6 +223,8 @@ function resetGame() {
     aCnt = 0;
     bCnt = 0;
     cCnt = 0;
+    hide = false;
+    window.alert('reset');
 }
 
 function aList(arry) {
@@ -501,7 +483,7 @@ function imgAnimate() {
     
     $('img').animate({
         opacity: 0
-    }, 1000).promise().done(function() {
+    }, 400).promise().done(function() {
         imgSel();
     });
 }
