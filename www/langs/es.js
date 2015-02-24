43953be3642664cb450c950d149ff927b981886d
Christopher Franklin <chrisf0321@gmail.com>
2015-02-24 15:29:28 -0500

    lang7

--- www/langs/es.js
+++ www/langs/es.js
@@ -4,10 +4,10 @@ var language = {
     "How to Play" : "Cómo jugar",
     "Crisis Support" : "Apoyo de Crisis",
     "Disclaimer" : "renuncia",
-    "Current Score:" : "Puntuación actual:",
-    "High Score:" : "Puntuación Récord:",
-    "Games Played:" : "Juegos jugados:",
-    "Overall Points:" : "Puntos totales:",
-    "Average Points:" : "Promedio de puntos:"
+    "Current Score: " : "Puntuación actual: ",
+    "High Score: " : "Puntuación Récord: ",
+    "Games Played: " : "Juegos jugados: ",
+    "Overall Points: " : "Puntos totales: ",
+    "Average Points: " : "Promedio de puntos: "
 };
 
