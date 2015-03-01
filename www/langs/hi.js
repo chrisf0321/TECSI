b327d5331206ef23a81317df3ace438aa4373474
Christopher Franklin <chrisf0321@gmail.com>
2015-03-01 17:49:16 -0500

    hi_lang

---
+++ www/langs/hi.js
@@ -0,0 +1,99 @@
+var aArry = ["img/a1.jpg", "img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a5.jpg", "img/a6.jpg", "img/a7.jpg",
+            "img/a8.jpg", "img/a9.jpg", "img/a10.jpg", "img/a11.jpg", "img/a12.jpg", "img/a13.jpg", "img/a14.jpg",
+            "img/a15.jpg", "img/a16.jpg", "img/a17.jpg", "img/a18.jpg", "img/a19.jpg", "img/a20.jpg", "img/a21.jpg",
+            "img/a22.jpg", "img/a23.jpg", "img/a24.jpg", "img/a25.jpg", "img/a26.jpg", "img/a27.jpg", "img/a28.jpg",
+            "img/a29.jpg", "img/a30.jpg", "img/hi/a31.jpg", "img/hi/a32.jpg", "img/hi/a33.jpg", "img/hi/a34.jpg", "img/hi/a35.jpg", 
+            "img/hi/a36.jpg", "img/hi/a37.jpg", "img/hi/a38.jpg", "img/hi/a39.jpg", "img/hi/a40.jpg", "img/hi/a41.jpg", "img/hi/a42.jpg", 
+            "img/hi/a43.jpg", "img/hi/a44.jpg", "img/hi/a45.jpg"];
+        
+var bArry = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg", "img/b5.jpg", "img/b6.jpg", "img/b7.jpg",
+            "img/b8.jpg", "img/b9.jpg", "img/b10.jpg", "img/b11.jpg", "img/b12.jpg", "img/b13.jpg", "img/b14.jpg",
+            "img/b15.jpg", "img/b17.jpg", "img/b18.jpg", "img/b19.jpg", "img/b20.jpg", "img/b21.jpg",
+            "img/b22.jpg", "img/b23.jpg", "img/b24.jpg", "img/b25.jpg", "img/b26.jpg", "img/b27.jpg", "img/b28.jpg",
+            "img/b29.jpg", "img/b30.jpg", "img/b31.jpg", "img/b32.jpg", "img/b33.jpg", "img/b34.jpg", "img/b35.jpg", 
+            "img/b36.jpg", "img/b37.jpg", "img/b38.jpg", "img/b41.jpg", "img/b42.jpg", 
+            "img/b43.jpg", "img/b44.jpg", "img/b46.jpg", "img/b47.jpg", "img/b48.jpg", "img/b49.jpg", 
+            "img/hi/b52.jpg", "img/hi/b53.jpg", "img/hi/b54.jpg", "img/hi/b55.jpg", "img/hi/b56.jpg", 
+            "img/hi/b57.jpg", "img/hi/b58.jpg", "img/hi/b59.jpg", "img/hi/b60.jpg", "img/hi/b61.jpg", "img/hi/b62.jpg", "img/hi/b63.jpg", 
+            "img/hi/b64.jpg", "img/hi/b65.jpg", "img/hi/b66.jpg", "img/hi/b67.jpg", "img/hi/b68.jpg", "img/hi/b69.jpg", "img/hi/b70.jpg"];
+        
+var cArry = ["img/c1.jpg", "img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg", "img/c6.jpg", "img/c7.jpg",
+            "img/c8.jpg", "img/c9.jpg", "img/c10.jpg", "img/c11.jpg", "img/c12.jpg", "img/c13.jpg", "img/c14.jpg",
+            "img/c15.jpg", "img/c16.jpg", "img/c17.jpg", "img/c18.jpg", "img/c19.jpg", "img/c20.jpg", "img/c21.jpg",
+            "img/c22.jpg", "img/c23.jpg", "img/c24.jpg", "img/c25.jpg", "img/c26.jpg", "img/c27.jpg", "img/c28.jpg",
+            "img/c29.jpg", "img/c30.jpg", "img/c31.jpg", "img/c32.jpg", "img/c33.jpg", "img/c34.jpg", "img/c35.jpg", 
+            "img/c36.jpg", "img/c37.jpg", "img/c38.jpg", "img/c39.jpg", "img/c40.jpg", "img/c41.jpg", "img/c42.jpg", 
+            "img/c43.jpg", "img/c44.jpg", "img/c45.jpg", "img/c46.jpg", "img/c47.jpg", "img/c48.jpg", "img/c49.jpg", 
+            "img/c50.jpg", "img/c51.jpg", "img/c52.jpg", "img/c53.jpg", "img/c54.jpg", "img/c55.jpg", "img/c56.jpg", 
+            "img/c57.jpg", "img/c58.jpg", "img/c59.jpg", "img/c60.jpg", "img/c61.jpg", "img/c62.jpg"];
+        
+var dArry = ["img/d1.jpg", "img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d5.jpg", "img/d6.jpg", "img/d7.jpg",
+            "img/d8.jpg", "img/d9.jpg", "img/d10.jpg", "img/d11.jpg", "img/d12.jpg", "img/d13.jpg", "img/d14.jpg",
+            "img/d15.jpg", "img/d16.jpg", "img/d17.jpg", "img/d18.jpg", "img/d19.jpg", "img/d20.jpg", "img/d21.jpg",
+            "img/d22.jpg", "img/d23.jpg", "img/d24.jpg", "img/d25.jpg", "img/d26.jpg", "img/d27.jpg", "img/d28.jpg",
+            "img/d29.jpg", "img/d30.jpg", "img/d31.jpg", "img/d32.jpg", "img/d33.jpg", "img/d34.jpg", "img/d35.jpg", 
+            "img/d36.jpg", "img/d37.jpg", "img/d38.jpg", "img/d39.jpg", "img/d40.jpg", "img/d41.jpg", "img/d42.jpg", 
+            "img/d43.jpg", "img/d44.jpg", "img/d45.jpg", "img/d46.jpg", "img/d47.jpg", "img/d48.jpg", "img/d49.jpg", 
+            "img/d50.jpg", "img/d51.jpg", "img/d52.jpg", "img/d53.jpg", "img/d54.jpg", "img/d55.jpg", "img/d56.jpg", 
+            "img/d57.jpg"];
+
+var eArry = ["img/hi/e1.jpg", "img/hi/e2.jpg", "img/hi/e3.jpg", "img/hi/e4.jpg"];
+
+$('#r1, #r2, #r3, #r4, #ing4').attr('src', eArry[0]);
+
+var language = {
+    "Tec-Tec" : "टीईसी-टेक",
+    "rld" : "भार वाम: ",
+    "gmScr" : "खेल स्कोर: ",
+    "Play Game!" : "खेल खेलना!",
+    "How to Play" : "कैसे खेलें",
+    "Crisis Support" : "संकट समर्थन",
+    "Disclaimer" : "त्याग",
+    "Current Score: " : "वर्तमान स्कोर: ",
+    "High Score: " : "उच्च स्कोर: ",
+    "Games Played: " : "खेले गए खेल: ",
+    "Overall Points: " : "कुल मिलाकर अंक: ",
+    "Average Points: " : "औसत अंक: ",
+    "gmStats" : "खेल आँकड़े",
+    "back" : "वापस",
+    "Resources" : "साधन",
+    "res1" : "आप आत्महत्या महसूस कर रहे हैं, कृपया संपर्क करें:",
+    "res2" : "अमेरिकी उपयोगकर्ताओं",
+    "res3" : "आत्महत्या निवारण हॉटलाइन: ",
+    "res4" : "रेखा आत्महत्या हॉटलाइन आशा है कि: ",
+    "res5" : "बुला ",
+    "res6" : "अमेरिकी और गैर-अमेरिकी उपयोगकर्ताओं",
+    "res7" : "आपका स्थानीय मनोरोग अस्पताल के वॉक-इन क्लिनिक",
+    "res8" : "अपने स्थानीय अस्पताल के आपातकालीन कक्ष",
+    "res9" : "आपका स्थानीय तत्काल देखभाल केंद्र या क्लिनिक",
+    "res10" : "अंतर्राष्ट्रीय आत्महत्या हॉटलाइन",
+    "res11" : "अंतर्राष्ट्रीय आत्महत्या संकट केंद्र की जानकारी",
+    "res12" : "अंतर्राष्ट्रीय आत्महत्या संकट लाइनों",
+    "inst" : "अनुदेश",
+    "inst1" : "टीईसी-टेक एक मिलान खेल है",
+    "inst2" : "लक्ष्य के लिए इस तरह एक ग्रिड के भीतर एक और छवि / शब्द के साथ एक छवि / शब्द मिलान करने के लिए है:",
+    "inst4" : "कैसे आप मैच के लिए क्या जानते हो?",
+    "inst5" : "प्रत्येक खेल के शुरू में, अगर आप इस तरह की छवियों / शब्दों के तीन जोड़े, दिया रहे हैं:",
+    "inst7" : "आप कैसे जोड़े से मेल करते हैं?",
+    "inst8" : "उन जोड़े में से एक प्रत्येक खेल स्क्रीन पर दिखाई देगा। वहां ",
+    "inst9" : " कुल खेल स्क्रीन।",
+    "inst10" : "जितनी जल्दी संभव जोड़ी के सदस्यों पर टैप करें। ",
+    "inst11" : "और तेज",
+    "inst12" : " और अधिक ",
+    "inst13" : "शुद्ध",
+    "inst14" : " यदि आप अधिक अंक आप मिलता है।",
+    "inst16" : "टीईसी-टेक और अधिक चुनौतीपूर्ण हो जाता है",
+    "inst17" : "छवि / शब्द विकल्प आप पहली छवि / शब्द का चयन करने के बाद बाहर काला करने के लिए शुरू:",
+    "inst18" : "तब टेक-टेक और भी चुनौतीपूर्ण हो जाता है",
+    "inst19" : "ग्रिड आकार में दोगुना हो जाता है:",
+    "finish" : "समाप्त हो गया!",
+    "main" : "मुख्य मेन्यू",
+    "matches" : "माचिस",
+    "start" : "खेल शुरू!",
+    "pair" : "जोडी "
+};
+
+
+
+
+
