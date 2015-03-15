4270c1a8ff64e948552e9ad27e52c8a23ee66cd0
Christopher Franklin <chrisf0321@gmail.com>
2015-03-15 16:03:57 -0400

    new_langs

---
+++ www/langs/ja.js
@@ -0,0 +1,99 @@
+var aArry = ["img/a1.jpg", "img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a5.jpg", "img/a6.jpg", "img/a7.jpg",
+            "img/a8.jpg", "img/a9.jpg", "img/a10.jpg", "img/a11.jpg", "img/a12.jpg", "img/a13.jpg", "img/a14.jpg",
+            "img/a15.jpg", "img/a16.jpg", "img/a17.jpg", "img/a18.jpg", "img/a19.jpg", "img/a20.jpg", "img/a21.jpg",
+            "img/a22.jpg", "img/a23.jpg", "img/a24.jpg", "img/a25.jpg", "img/a26.jpg", "img/a27.jpg", "img/a28.jpg",
+            "img/a29.jpg", "img/a30.jpg", "img/a31.jpg", "img/a32.jpg", "img/a33.jpg", "img/a34.jpg", "img/a35.jpg", 
+            "img/a36.jpg", "img/a37.jpg", "img/a38.jpg", "img/a39.jpg", "img/a40.jpg", "img/a41.jpg", "img/a42.jpg", 
+            "img/a43.jpg", "img/a44.jpg", "img/a45.jpg", "img/a46.jpg", "img/a47.jpg", "img/a48.jpg", "img/a49.jpg"];
+        
+var bArry = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg", "img/b5.jpg", "img/b6.jpg", "img/b7.jpg",
+            "img/b8.jpg", "img/b9.jpg", "img/b10.jpg", "img/b11.jpg", "img/b12.jpg", "img/b13.jpg", "img/b14.jpg",
+            "img/b15.jpg", "img/b16.jpg", "img/b17.jpg", "img/b18.jpg", "img/b19.jpg", "img/b20.jpg", "img/b21.jpg",
+            "img/b22.jpg", "img/b23.jpg", "img/b24.jpg", "img/b25.jpg", "img/b26.jpg", "img/b27.jpg", "img/b28.jpg",
+            "img/b29.jpg", "img/b30.jpg", "img/b31.jpg", "img/b32.jpg", "img/b33.jpg", "img/b34.jpg", "img/b35.jpg", 
+            "img/b36.jpg", "img/b37.jpg", "img/b38.jpg", "img/b39.jpg", "img/b40.jpg", "img/b41.jpg", "img/b42.jpg", 
+            "img/b43.jpg", "img/b44.jpg", "img/b45.jpg", "img/b46.jpg", "img/b47.jpg", "img/b48.jpg", "img/b49.jpg", 
+            "img/b50.jpg", "img/b51.jpg", "img/b52.jpg", "img/b53.jpg", "img/b54.jpg", "img/b55.jpg", "img/b56.jpg", 
+            "img/b57.jpg", "img/b58.jpg", "img/b59.jpg", "img/b60.jpg", "img/b61.jpg", "img/b62.jpg", "img/b63.jpg", 
+            "img/b64.jpg", "img/b65.jpg", "img/b66.jpg", "img/b67.jpg", "img/b68.jpg", "img/b69.jpg", "img/b70.jpg"];
+        
+var cArry = ["img/c1.jpg", "img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg", "img/c6.jpg", "img/c7.jpg",
+            "img/c8.jpg", "img/c9.jpg", "img/c10.jpg", "img/c11.jpg", "img/c12.jpg", "img/c13.jpg", "img/c14.jpg",
+            "img/c15.jpg", "img/c16.jpg", "img/c17.jpg", "img/c18.jpg", "img/c19.jpg", "img/c20.jpg", "img/c21.jpg",
+            "img/c22.jpg", "img/c23.jpg", "img/c24.jpg", "img/c25.jpg", "img/c26.jpg", "img/c27.jpg", "img/c28.jpg",
+            "img/c29.jpg", "img/c30.jpg", "img/c31.jpg", "img/c32.jpg", "img/c33.jpg", "img/c34.jpg", "img/c35.jpg", 
+            "img/c36.jpg", "img/c37.jpg", "img/c38.jpg", "img/c39.jpg", "img/c40.jpg", "img/c41.jpg", "img/c42.jpg", 
+            "img/c43.jpg", "img/c44.jpg", "img/c45.jpg", "img/c46.jpg", "img/c47.jpg", "img/c48.jpg", "img/c49.jpg", 
+            "img/c50.jpg", "img/c51.jpg", "img/c52.jpg", "img/c53.jpg", "img/c54.jpg", "img/c55.jpg", "img/c56.jpg", 
+            "img/c57.jpg", "img/c58.jpg", "img/c59.jpg", "img/c60.jpg", "img/c61.jpg", "img/c62.jpg"];
+        
+var dArry = ["img/d1.jpg", "img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d5.jpg", "img/d6.jpg", "img/d7.jpg",
+            "img/d8.jpg", "img/d9.jpg", "img/d10.jpg", "img/d11.jpg", "img/d12.jpg", "img/d13.jpg", "img/d14.jpg",
+            "img/d15.jpg", "img/d16.jpg", "img/d17.jpg", "img/d18.jpg", "img/d19.jpg", "img/d20.jpg", "img/d21.jpg",
+            "img/d22.jpg", "img/d23.jpg", "img/d24.jpg", "img/d25.jpg", "img/d26.jpg", "img/d27.jpg", "img/d28.jpg",
+            "img/d29.jpg", "img/d30.jpg", "img/d31.jpg", "img/d32.jpg", "img/d33.jpg", "img/d34.jpg", "img/d35.jpg", 
+            "img/d36.jpg", "img/d37.jpg", "img/d38.jpg", "img/d39.jpg", "img/d40.jpg", "img/d41.jpg", "img/d42.jpg", 
+            "img/d43.jpg", "img/d44.jpg", "img/d45.jpg", "img/d46.jpg", "img/d47.jpg", "img/d48.jpg", "img/d49.jpg", 
+            "img/d50.jpg", "img/d51.jpg", "img/d52.jpg", "img/d53.jpg", "img/d54.jpg", "img/d55.jpg", "img/d56.jpg", 
+            "img/d57.jpg"];
+
+var eArry = ["img/e1.jpg", "img/e2.jpg", "img/e3.jpg", "img/e4.jpg"];
+
+$('#r1, #r2, #r3, #r4, #ing4').attr('src', eArry[0]);
+
+var language = {
+    "Tec-Tec" : "TEC-テック",
+    "rld" : "リロード左： ",
+    "gmScr" : "ゲームのスコア： ",
+    "Play Game!" : "ゲームをプレイ！",
+    "How to Play" : "プレイ方法",
+    "Crisis Support" : "危機サポート",
+    "Disclaimer" : "免責事項",
+    "Current Score: " : "現在のスコア： ",
+    "High Score: " : "ハイスコア： ",
+    "Games Played: " : "プレイしたゲーム： ",
+    "Overall Points: " : "全体的なポイント： ",
+    "Average Points: " : "平均ポイント： ",
+    "gmStats" : "ゲームの統計",
+    "back" : "バック",
+    "Resources" : "リソース",
+    "res1" : "あなたが自殺を感じている場合は、お問い合わせください。",
+    "res2" : "アメリカのユーザー",
+    "res3" : "自殺防止ホットライン： ",
+    "res4" : "ライン自殺ホットラインホープ： ",
+    "res5" : "コール ",
+    "res6" : "アメリカ人と非アメリカ人のユーザー",
+    "res7" : "お近くの精神科病院のウォークインクリニック",
+    "res8" : "あなたの地元の病院の緊急治療室",
+    "res9" : "あなたのローカル緊急のケアセンターや診療所",
+    "res10" : "国際自殺ホットライン",
+    "res11" : "国際自殺危機センター情報",
+    "res12" : "国際自殺の危機ライン",
+    "inst" : "説明書",
+    "inst1" : "TEC-テックは、マッチングゲームです",
+    "inst2" : "目標は、このようなグリッド内の別の画像/ワードで1画像/ワードと一致することです：",
+    "inst4" : "どのようにあなたは何に一致するように知っているのですか？",
+    "inst5" : "各ゲームの開始時に、あなたは、このような画像/言葉の3ペアを、与えられている：",
+    "inst7" : "どのようにペアを一致させるのですか？",
+    "inst8" : "それらのペアの一つは、各ゲーム画面に表示されます。がある ",
+    "inst9" : " 総ゲーム画面。",
+    "inst10" : "可能な限り迅速に対のメンバーをタップします。ザ· ",
+    "inst11" : "もっと早く",
+    "inst12" : " より ",
+    "inst13" : "正確な",
+    "inst14" : " あなたは、あなたが得るより多くのポイントです。",
+    "inst16" : "TEC-テックは、より挑戦的な取得します",
+    "inst17" : "画像/ワード·オプションを使用すると、最初の画像/単語を選択した後に黒に起動します。",
+    "inst18" : "その後、TEC-テックはさらに困難を取得",
+    "inst19" : "グリッドのサイズは2倍に。",
+    "finish" : "完成！",
+    "main" : "メインメニュー",
+    "matches" : "マッチ",
+    "start" : "ゲームを開始！",
+    "pair" : "ペア "
+};
+
+
+
+
+
---
+++ www/langs/zht.js
@@ -0,0 +1,98 @@
+var aArry = ["img/a1.jpg", "img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a5.jpg", "img/a6.jpg", "img/a7.jpg",
+            "img/a8.jpg", "img/a9.jpg", "img/a10.jpg", "img/a11.jpg", "img/a12.jpg", "img/a13.jpg", "img/a14.jpg",
+            "img/a15.jpg", "img/a16.jpg", "img/a17.jpg", "img/a18.jpg", "img/a19.jpg", "img/a20.jpg", "img/a21.jpg",
+            "img/a22.jpg", "img/a23.jpg", "img/a24.jpg", "img/a25.jpg", "img/a26.jpg", "img/a27.jpg", "img/a28.jpg",
+            "img/a29.jpg", "img/a30.jpg", "img/a31.jpg", "img/a32.jpg", "img/a33.jpg", "img/a34.jpg", "img/a35.jpg", 
+            "img/a36.jpg", "img/a37.jpg", "img/a38.jpg", "img/a39.jpg", "img/a40.jpg", "img/a41.jpg", "img/a42.jpg", 
+            "img/a43.jpg", "img/a44.jpg", "img/a45.jpg", "img/a46.jpg", "img/a47.jpg", "img/a48.jpg", "img/a49.jpg"];
+        
+var bArry = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg", "img/b5.jpg", "img/b6.jpg", "img/b7.jpg",
+            "img/b8.jpg", "img/b9.jpg", "img/b10.jpg", "img/b11.jpg", "img/b12.jpg", "img/b13.jpg", "img/b14.jpg",
+            "img/b15.jpg", "img/b16.jpg", "img/b17.jpg", "img/b18.jpg", "img/b19.jpg", "img/b20.jpg", "img/b21.jpg",
+            "img/b22.jpg", "img/b23.jpg", "img/b24.jpg", "img/b25.jpg", "img/b26.jpg", "img/b27.jpg", "img/b28.jpg",
+            "img/b29.jpg", "img/b30.jpg", "img/b31.jpg", "img/b32.jpg", "img/b33.jpg", "img/b34.jpg", "img/b35.jpg", 
+            "img/b36.jpg", "img/b37.jpg", "img/b38.jpg", "img/b39.jpg", "img/b40.jpg", "img/b41.jpg", "img/b42.jpg", 
+            "img/b43.jpg", "img/b44.jpg", "img/b45.jpg", "img/b46.jpg", "img/b47.jpg", "img/b48.jpg", "img/b49.jpg", 
+            "img/b50.jpg", "img/b51.jpg", "img/b52.jpg", "img/b53.jpg", "img/b54.jpg", "img/b55.jpg", "img/b56.jpg", 
+            "img/b57.jpg", "img/b58.jpg", "img/b59.jpg", "img/b60.jpg", "img/b61.jpg", "img/b62.jpg", "img/b63.jpg", 
+            "img/b64.jpg", "img/b65.jpg", "img/b66.jpg", "img/b67.jpg", "img/b68.jpg", "img/b69.jpg", "img/b70.jpg"];
+        
+var cArry = ["img/c1.jpg", "img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg", "img/c6.jpg", "img/c7.jpg",
+            "img/c8.jpg", "img/c9.jpg", "img/c10.jpg", "img/c11.jpg", "img/c12.jpg", "img/c13.jpg", "img/c14.jpg",
+            "img/c15.jpg", "img/c16.jpg", "img/c17.jpg", "img/c18.jpg", "img/c19.jpg", "img/c20.jpg", "img/c21.jpg",
+            "img/c22.jpg", "img/c23.jpg", "img/c24.jpg", "img/c25.jpg", "img/c26.jpg", "img/c27.jpg", "img/c28.jpg",
+            "img/c29.jpg", "img/c30.jpg", "img/c31.jpg", "img/c32.jpg", "img/c33.jpg", "img/c34.jpg", "img/c35.jpg", 
+            "img/c36.jpg", "img/c37.jpg", "img/c38.jpg", "img/c39.jpg", "img/c40.jpg", "img/c41.jpg", "img/c42.jpg", 
+            "img/c43.jpg", "img/c44.jpg", "img/c45.jpg", "img/c46.jpg", "img/c47.jpg", "img/c48.jpg", "img/c49.jpg", 
+            "img/c50.jpg", "img/c51.jpg", "img/c52.jpg", "img/c53.jpg", "img/c54.jpg", "img/c55.jpg", "img/c56.jpg", 
+            "img/c57.jpg", "img/c58.jpg", "img/c59.jpg", "img/c60.jpg", "img/c61.jpg", "img/c62.jpg"];
+        
+var dArry = ["img/d1.jpg", "img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d5.jpg", "img/d6.jpg", "img/d7.jpg",
+            "img/d8.jpg", "img/d9.jpg", "img/d10.jpg", "img/d11.jpg", "img/d12.jpg", "img/d13.jpg", "img/d14.jpg",
+            "img/d15.jpg", "img/d16.jpg", "img/d17.jpg", "img/d18.jpg", "img/d19.jpg", "img/d20.jpg", "img/d21.jpg",
+            "img/d22.jpg", "img/d23.jpg", "img/d24.jpg", "img/d25.jpg", "img/d26.jpg", "img/d27.jpg", "img/d28.jpg",
+            "img/d29.jpg", "img/d30.jpg", "img/d31.jpg", "img/d32.jpg", "img/d33.jpg", "img/d34.jpg", "img/d35.jpg", 
+            "img/d36.jpg", "img/d37.jpg", "img/d38.jpg", "img/d39.jpg", "img/d40.jpg", "img/d41.jpg", "img/d42.jpg", 
+            "img/d43.jpg", "img/d44.jpg", "img/d45.jpg", "img/d46.jpg", "img/d47.jpg", "img/d48.jpg", "img/d49.jpg", 
+            "img/d50.jpg", "img/d51.jpg", "img/d52.jpg", "img/d53.jpg", "img/d54.jpg", "img/d55.jpg", "img/d56.jpg", 
+            "img/d57.jpg"];
+
+var eArry = ["img/e1.jpg", "img/e2.jpg", "img/e3.jpg", "img/e4.jpg"];
+
+$('#r1, #r2, #r3, #r4, #ing4').attr('src', eArry[0]);
+
+var language = {
+    "Tec-Tec" : "Tec-Tec",
+    "rld" : "重載左： ",
+    "gmScr" : "遊戲評分： ",
+    "Play Game!" : "玩遊戲！",
+    "How to Play" : "如何玩",
+    "Crisis Support" : "危機支援",
+    "Disclaimer" : "放棄",
+    "Current Score: " : "當前得分： ",
+    "High Score: " : "高分： ",
+    "Games Played: " : "玩過遊戲： ",
+    "Overall Points: " : "總體積分： ",
+    "Average Points: " : "平均分： ",
+    "gmStats" : "比賽統計",
+    "back" : "背部",
+    "Resources" : "資源",
+    "res1" : "如果你是想自殺，請聯繫：",
+    "res2" : "美國用戶",
+    "res3" : "預防自殺熱線： ",
+    "res4" : "希望行自殺熱線： ",
+    "res5" : "通話 ",
+    "res6" : "美國和非美國用戶",
+    "res7" : "當地的精神病醫院的步入式診所",
+    "res8" : "您當地醫院的急診室",
+    "res9" : "當地的緊急護理中心或診所",
+    "res10" : "國際自殺熱線",
+    "res11" : "國際自殺危機處理中心信息",
+    "res12" : "國際自殺危機線",
+    "inst" : "說明",
+    "inst1" : "TEC-Tec的是一個配對遊戲",
+    "inst2" : "我們的目標是一致的一個圖像/字具有這樣的網格內的另一圖像/詞語：",
+    "inst4" : "你怎麼知道該怎麼搭配？",
+    "inst5" : "在每場比賽開始，你給3雙圖像/文字，就像這樣：",
+    "inst7" : "你如何搭配雙？",
+    "inst8" : "其中一個對將出現在每個遊戲的畫面上。有 ",
+    "inst9" : " 總的遊戲畫面。",
+    "inst10" : "輕按一對成員盡快。該 ",
+    "inst11" : "快點",
+    "inst12" : " 多 ",
+    "inst13" : "準確",
+    "inst14" : " 你是，更多的積分，你得到的。",
+    "inst16" : "TEC-Tec的變得更具挑戰性",
+    "inst17" : "圖片/ Word選項開始黑您選擇的第一張圖像/字出來後：",
+    "inst18" : "那麼TEC-Tec的會變得更具挑戰性",
+    "inst19" : "網格的大小加倍：",
+    "finish" : "完了！",
+    "main" : "主菜單",
+    "matches" : "火柴",
+    "start" : "開始遊戲！",
+    "pair" : "對 "
+};
+
+
+
+
